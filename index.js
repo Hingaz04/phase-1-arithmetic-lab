ead5d34c7cdd499d3a6a2a4f1fc9a7a980e497df
Hingaz <hhinga04@gmail.com>
2023-09-23 21:32:37 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 // Write your code here
+
+ Math.random()
+ 
+const num3 = 8;
+const num4 = 2;
+const mod = num3 / num4;
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+
+const numbers = [1, 14, 19, 10, 20, 13];
+const max = Math.max(...numbers);
+
+ 
+
+ 
